c5719da1abbb12760daaefb8669c631a17d0c879
zhangxinmeng1 <zhangxinmeng1@faw.com.cn>
2022-05-22 19:17:44 +0800

    first init

---
+++ app/src/main/java/com/enjoy/enjoyclass04/api/EnjoyWeatherApi.java
@@ -0,0 +1,19 @@
+package com.enjoy.enjoyclass04.api;
+
+
+import com.enjoy.enjoyclass04.retrofit.annotation.Field;
+import com.enjoy.enjoyclass04.retrofit.annotation.GET;
+import com.enjoy.enjoyclass04.retrofit.annotation.POST;
+import com.enjoy.enjoyclass04.retrofit.annotation.Query;
+
+import okhttp3.Call;
+
+public interface EnjoyWeatherApi {
+
+    @POST("/v3/weather/weatherInfo")
+    Call postWeather(@Field("city") String city, @Field("key") String key);
+
+
+    @GET("/v3/weather/weatherInfo")
+    Call getWeather(@Query("city") String city, @Query("key") String key);
+}
